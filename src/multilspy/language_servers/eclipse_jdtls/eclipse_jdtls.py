7692f426411627612db77aeec975fd4c3b720d44
Stephen Rayner <contact@gruckion.com>
2026-04-10 02:08:50 +0000

    Update JavaSE-17 references to JavaSE-21 for JDTLS JRE upgrade
    The JRE bundled with vscode-java was upgraded from Java 17 to Java 21
    in #126, but the JavaSE runtime name references in eclipse_jdtls.py
    and initialize_params.json were not updated. This causes an assertion
    failure at runtime because the assert expects JavaSE-17 in the config
    but the JSON now specifies Java 21 paths.
    
    Caught by @gruckion in #128.

--- src/multilspy/language_servers/eclipse_jdtls/eclipse_jdtls.py
+++ src/multilspy/language_servers/eclipse_jdtls/eclipse_jdtls.py
@@ -276,10 +276,10 @@ class EclipseJDTLS(LanguageServer):
         d["initializationOptions"]["bundles"] = bundles
 
         assert d["initializationOptions"]["settings"]["java"]["configuration"]["runtimes"] == [
-            {"name": "JavaSE-17", "path": "static/vscode-java/extension/jre/17.0.8.1-linux-x86_64", "default": True}
+            {"name": "JavaSE-21", "path": "static/vscode-java/extension/jre/17.0.8.1-linux-x86_64", "default": True}
         ]
         d["initializationOptions"]["settings"]["java"]["configuration"]["runtimes"] = [
-            {"name": "JavaSE-17", "path": self.runtime_dependency_paths.jre_home_path, "default": True}
+            {"name": "JavaSE-21", "path": self.runtime_dependency_paths.jre_home_path, "default": True}
         ]
 
         for runtime in d["initializationOptions"]["settings"]["java"]["configuration"]["runtimes"]:
